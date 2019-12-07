446e9bb0a624753ddcee3900360293e530488646
Arnar Freyr Kristinsson <arnar11@hotmail.com>
2019-12-07 12:42:44 +0000

    deploy

--- scripts/jenkins_deploy.sh
+++ scripts/jenkins_deploy.sh
@@ -23,7 +23,7 @@ cd /var/lib/jenkins/terraform/hgop/production
 ./terraform destroy -auto-approve
 ./terraform apply -auto-approve
 
-echo "Game API running at " + $(terraform output public_ip)
+echo "Game API running at " + $(./terraform output public_ip)
 
 ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(./terraform output public_ip) "./initialize_game_api_instance.sh"
 ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(./terraform output public_ip) "./docker_compose_up.sh $GIT_COMMIT"
