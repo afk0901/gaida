a4fc81a7d10eadf94eb9cfcf17188246018777fe
Arnar Freyr Kristinsson <arnar11@hotmail.com>
2019-12-10 03:56:30 +0000

    deployscript update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #Moving to the directory where Terraform is installed and its configuration files
 cd ..
+set -euxo pipefail
 ./terraform init #Initalize Terraform so its possible to use it
 ./terraform destroy -auto-approve #Destroys all instances managed by Terraform
 ./terraform plan #Show the execution plan
 ./terraform apply -auto-approve #Apply the new instance
 #Run the initialization script on the instance
 ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(./terraform output public_ip) "./initialize_game_api_instance.sh"
+curl $(terraform output public_ip):3000/status
 
 
