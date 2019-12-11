5248b4cb8fd5b69b65820617f8ba500e9a394d09
Arnar Freyr Kristinsson <arnar11@hotmail.com>
2019-12-11 18:22:02 +0000

    TERRAFORM

--- scripts/initialize_game_api_instance.sh
+++ scripts/initialize_game_api_instance.sh
@@ -24,7 +24,7 @@ sudo usermod -aG docker ubuntu
 
 echo 'Install Docker Compose'
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
 
 # Check if docker-compose.yml file is present or not.
 if [ ! -f ./docker-compose.yml ]; then
