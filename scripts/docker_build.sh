55829e28601c78997f849cac2d5de29272084d72
Arnar Freyr Kristinsson <arnar11@hotmail.com>
2019-12-11 20:19:22 +0000

    m

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-ARG GIT_COMMIT=$1
+GIT_COMMIT=$1
 
 docker build -t afk0901/game_api_repoitory:$GITCOMMIT game_api/
 
--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-ARG GIT_COMMIT=$1
+GIT_COMMIT=$1
 
 docker push afk0901/game_api_repoitory:$GIT_COMMIT
 
