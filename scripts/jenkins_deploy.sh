bad05a9bae552787de87151b66a46f3eb4a66082
Arnar Freyr Kristinsson <arnar11@hotmail.com>
2019-12-12 15:38:18 +0000

    newfolder

--- scripts/jenkins_deploy.sh
+++ scripts/jenkins_deploy.sh
@@ -20,9 +20,10 @@ rm -f /var/lib/jenkins/terraform/hgop/$2/*.tf
 cp ./*.tf /var/lib/jenkins/terraform/hgop/$2/
 cp ./terraform /var/lib/jenkins/terraform/hgop/$2/
 
-ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(./terraform output public_ip) "mkdir -p /home/ubuntu/game_api"
+
 
 cd /var/lib/jenkins/terraform/hgop/$2
+ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(./terraform output public_ip) "mkdir /home/ubuntu/game_api"
 ./terraform init # In case terraform is not initialized
 ./terraform force-unlock
 ./terraform destroy -auto-approve -var environment=$2 || exit 1
