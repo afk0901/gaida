0604603d71ac8d2efed7363a31c7cecf79234a78
Arnar Freyr Kristinsson <arnar11@hotmail.com>
2019-12-12 01:54:44 +0000

    Deploy paramter

--- scripts/jenkins_deploy.sh
+++ scripts/jenkins_deploy.sh
@@ -4,25 +4,25 @@
 GIT_COMMIT=$1
 
 # We need to move some files around, because of the terraform state limitations.
-mkdir -p /var/lib/jenkins/terraform/hgop/production
-mkdir -p /var/lib/jenkins/terraform/hgop/production/scripts
-rm -f /var/lib/jenkins/terraform/hgop/production/scripts/initialize_game_api_instance.sh
-cp scripts/initialize_game_api_instance.sh /var/lib/jenkins/terraform/hgop/production/scripts/initialize_game_api_instance.sh
-rm -f /var/lib/jenkins/terraform/hgop/production/scripts/docker_compose_up.sh
-cp scripts/docker_compose_up.sh /var/lib/jenkins/terraform/hgop/production/scripts/docker_compose_up.sh
-rm -f /var/lib/jenkins/terraform/hgop/production/docker-compose.yml
-cp docker-compose.yml /var/lib/jenkins/terraform/hgop/production/docker-compose.yml
+mkdir -p /var/lib/jenkins/terraform/hgop/$2
+mkdir -p /var/lib/jenkins/terraform/hgop/$2/scripts
+rm -f /var/lib/jenkins/terraform/hgop/$2/scripts/initialize_game_api_instance.sh
+cp scripts/initialize_game_api_instance.sh /var/lib/jenkins/terraform/hgop/$2/scripts/initialize_game_api_instance.sh
+rm -f /var/lib/jenkins/terraform/hgop/$2/scripts/docker_compose_up.sh
+cp scripts/docker_compose_up.sh /var/lib/jenkins/terraform/hgop/$2/scripts/docker_compose_up.sh
+rm -f /var/lib/jenkins/terraform/hgop/$2/docker-compose.yml
+cp docker-compose.yml /var/lib/jenkins/terraform/hgop/$2/docker-compose.yml
 
 
-rm -f /var/lib/jenkins/terraform/hgop/production/*.tf
-cp ./*.tf /var/lib/jenkins/terraform/hgop/production/
-cp ./terraform /var/lib/jenkins/terraform/hgop/production/
+rm -f /var/lib/jenkins/terraform/hgop/$2/*.tf
+cp ./*.tf /var/lib/jenkins/terraform/hgop/$2/
+cp ./terraform /var/lib/jenkins/terraform/hgop/$2/
 
-cd /var/lib/jenkins/terraform/hgop/production
+cd /var/lib/jenkins/terraform/hgop/$2
 ./terraform init # In case terraform is not initialized.
 ./terraform force-unlock
-./terraform destroy -auto-approve -var environment=production || exit 1
-./terraform apply -auto-approve -var environment=production  || exit 1
+./terraform destroy -auto-approve -var environment=$2 || exit 1
+./terraform apply -auto-approve -var environment=$2  || exit 1
 
 echo "Game API running at " + $(./terraform output public_ip)
 
