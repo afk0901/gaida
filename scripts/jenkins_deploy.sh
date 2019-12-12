1295af33c66d45dc3f601b1c50857612ad3dab7c
Arnar Freyr Kristinsson <arnar11@hotmail.com>
2019-12-12 00:32:19 +0000

    apitest environment and path

--- scripts/jenkins_deploy.sh
+++ scripts/jenkins_deploy.sh
@@ -4,18 +4,19 @@
 GIT_COMMIT=$1
 
 # We need to move some files around, because of the terraform state limitations.
-mkdir -p /var/lib/jenkins/terraform/hgop/production
-mkdir -p /var/lib/jenkins/terraform/hgop/production/scripts
-rm -f /var/lib/jenkins/terraform/hgop/production/scripts/initialize_game_api_instance.sh
-cp scripts/initialize_game_api_instance.sh /var/lib/jenkins/terraform/hgop/production/scripts/initialize_game_api_instance.sh
-rm -f /var/lib/jenkins/terraform/hgop/production/scripts/docker_compose_up.sh
-cp scripts/docker_compose_up.sh /var/lib/jenkins/terraform/hgop/production/scripts/docker_compose_up.sh
-rm -f /var/lib/jenkins/terraform/hgop/production/docker-compose.yml
-cp docker-compose.yml /var/lib/jenkins/terraform/hgop/production/docker-compose.yml
+#$3 = environment
+mkdir -p /var/lib/jenkins/terraform/hgop/$3
+mkdir -p /var/lib/jenkins/terraform/hgop/$3/scripts
+rm -f /var/lib/jenkins/terraform/hgop/$3/scripts/initialize_game_api_instance.sh
+cp scripts/initialize_game_api_instance.sh /var/lib/jenkins/terraform/hgop/$3/scripts/initialize_game_api_instance.sh
+rm -f /var/lib/jenkins/terraform/hgop/$3/scripts/docker_compose_up.sh
+cp scripts/docker_compose_up.sh /var/lib/jenkins/terraform/hgop/$3/scripts/docker_compose_up.sh
+rm -f /var/lib/jenkins/terraform/hgop/$3/docker-compose.yml
+cp docker-compose.yml /var/lib/jenkins/terraform/hgop/$3/docker-compose.yml
 
 
-rm -f /var/lib/jenkins/terraform/hgop/production/*.tf
-cp ./*.tf /var/lib/jenkins/terraform/hgop/production/
+rm -f /var/lib/jenkins/terraform/hgop/$3/*.tf
+cp ./*.tf /var/lib/jenkins/terraform/hgop/$3/
 
 #cd into the Terraform path and set the environment
 cd $2
