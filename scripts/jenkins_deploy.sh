419f2928cb4987c9d515e83c46a1e8660d44b2b2
Arnar Freyr Kristinsson <arnar11@hotmail.com>
2019-12-13 00:59:43 +0000

    path problem - fix

--- scripts/jenkins_deploy.sh
+++ scripts/jenkins_deploy.sh
@@ -35,10 +35,12 @@ ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(./terraform
 #Runs api tests against the apitest instance
 if [ $2 == apitest ]
 then
+  cd /var/lib/jenkins/workspace/Pipeline_project/game_api
   API_URL = $(./terraform output public_ip)
   echo "API url is " + API_URL
   API_URL="${API_URL}:3000" npm run test:api
   ./terraform destroy -auto-approve -var environment=$2 || exit 1
+  cd - 
 fi  
 
 #Runs capacity tests against the capacitytest instance
