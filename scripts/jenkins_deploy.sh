28f9660909f1bf482724179401b0064ae93027f9
Arnar Freyr Kristinsson <arnar11@hotmail.com>
2019-12-13 01:47:29 +0000

    API url run accaptence test

--- scripts/jenkins_deploy.sh
+++ scripts/jenkins_deploy.sh
@@ -37,10 +37,11 @@ if [ $2 == apitest ]
 then
   
   API_URL=$(./terraform output public_ip)
-  echo "API url is ${API_URL}"
+  
   API_URL="${API_URL}:3000" 
   cd /var/lib/jenkins/workspace/Pipeline_project/game_api
-  npm run test:api
+  echo "API url is ${API_URL}"
+  API_URL npm run test:api
   #./terraform destroy -auto-approve -var environment=$2 || exit 1
 fi  
 
